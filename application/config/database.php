87971f300529131e05b5bd86f62ebf55346ee8cb
olatunjibolu <78906809+olatunjibolu@users.noreply.github.com>
2021-02-12 18:38:26 +0100

    Update database.php
    Created a default db config

--- application/config/database.php
+++ application/config/database.php
@@ -72,79 +72,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 //$active_group = 'default';
 
-switch(ENVIRONMENT)
-{
-	case 'development';
-	$active_group = 'development';
-	break;
-
-	case 'testing';
-	$active_group = 'testing';
-	break;
-
-	default;
-	$active_group = 'development';
-	break;
-
-
-
-}
-
+$active_group = 'default';
 $query_builder = TRUE;
 
-$db['production'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-
-
-$db['testing'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-
-$db['development'] = array(
+$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'hmvc_proj',
+	'database' => 'ticket',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
